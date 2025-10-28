da0aed92b8a879961b629fb4596a2026065ac5a5
paulmartincurry <paulmartincurry@gmail.com>
2025-10-28 12:55:37 -0700

    Create MiniPlayer.tsx

---
+++ components/MiniPlayer.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function MiniPlayer() {
+  return (
+    <div className="aspect-video w-full rounded-2xl overflow-hidden shadow-lg bg-gray-100 flex items-center justify-center">
+      <span>MiniPlayer placeholder</span>
+    </div>
+  );
+}
